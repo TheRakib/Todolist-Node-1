e1f484c2eb3bf2bd374b2940c8fc7929bb7e58be
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-03-08 12:17:34 +0100

    finished edit feature

--- controller/editController.js
+++ controller/editController.js
@@ -2,9 +2,21 @@ const Todo = require("../model/todo");
 const User = require("../model/user");
 
 const editRender = async (req, res) => {
-    res.render("editTodos.ejs");
+    const todo = await Todo.findOne({_id: req.params.id});
+    res.render("editTodos.ejs", {todo: todo});
+}
+
+const editPost = async (req, res) => {
+    try {
+        await Todo.updateOne({ _id: req.body.id, task: req.body.task});
+        res.redirect("/todo");
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = {
-    editRender
+    editRender,
+    editPost
 }
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -3,15 +3,19 @@ const todoRouter = express.Router();
 const Todo = require("../model/todo");
 const verifyUser = require("../middleware/verifyUser");
 
-const {editRender} = require("../controller/editController");
+const { editRender, editPost } = require("../controller/editController");
 
-const { todoRender, createTodo, deleteTodo } = require("../controller/todoController");
+const {
+  todoRender,
+  createTodo,
+  deleteTodo,
+} = require("../controller/todoController");
 
 todoRouter.get("/todo", verifyUser, todoRender);
 todoRouter.get("/delete/:id", verifyUser, deleteTodo);
 todoRouter.get("/edit/:id", verifyUser, editRender);
 
 todoRouter.post("/todo", verifyUser, createTodo);
- 
+todoRouter.post("/edit", verifyUser, editPost);
 
 module.exports = todoRouter;
